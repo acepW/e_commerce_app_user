18dd02231222e108a78822b1cb7a03e8bdcff9ea
acepW <acepwahyu001@gmail.com>
2023-12-12 11:07:37 +0700

    edit category and image home

--- lib/consts/contss.dart
+++ lib/consts/contss.dart
@@ -2,9 +2,9 @@ import 'package:e_commerce_app_user/models/products_model.dart';
 
 class Constss {
   static final List<String> offerImages = [
-    'assets/images/offers/fot1.jpeg',
-    'assets/images/offers/kuale1.jpeg',
-    'assets/images/offers/kuale2.jpeg',
+    'assets/images/content1.jpg',
+    'assets/images/content2.jpg',
+    'assets/images/content3.jpg',
   ];
   static List<String> authImagesPaths = [
     'assets/images/offers/fot1.jpeg',
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -13,27 +13,42 @@ class CategoriesScreen extends StatelessWidget {
     const Color(0xffD3B0E0),
     const Color(0xffFDE598),
     const Color(0xffB7DFF5),
+    const Color(0xffB7DFF5),
+    const Color(0xffB7DFF5),
+    const Color(0xffB7DFF5),
   ];
   List<Map<String, dynamic>> catInfo = [
     {
       'imgPath': 'assets/images/cat/fruits.png',
-      'catText': 'Elektronik',
+      'catText': 'Sayuran',
     },
     {
       'imgPath': 'assets/images/cat/veg.png',
-      'catText': 'Sparepart',
+      'catText': 'Ternak',
     },
     {
       'imgPath': 'assets/images/cat/nuts.png',
-      'catText': 'House',
+      'catText': 'Makanan',
     },
     {
       'imgPath': 'assets/images/cat/spices.png',
-      'catText': 'Clothes',
+      'catText': 'Minuman',
     },
     {
       'imgPath': 'assets/images/cat/grains.png',
-      'catText': 'Kitchen',
+      'catText': 'Bibit',
+    },
+    {
+      'imgPath': 'assets/images/cat/grains.png',
+      'catText': 'Alat',
+    },
+    {
+      'imgPath': 'assets/images/cat/grains.png',
+      'catText': 'Obat',
+    },
+    {
+      'imgPath': 'assets/images/cat/grains.png',
+      'catText': 'Jasa',
     },
     {
       'imgPath': 'assets/images/cat/Spinach.png',
@@ -60,10 +75,11 @@ class CategoriesScreen extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: GridView.count(
           crossAxisCount: 2,
+
           childAspectRatio: 240 / 250,
           crossAxisSpacing: 10, //vertical
           mainAxisSpacing: 10, //horizontal
-          children: List.generate(6, (index) {
+          children: List.generate(catInfo.length, (index) {
             return CategoriesWidget(
               catText: catInfo[index]['catText'],
               imgPath: catInfo[index]['imgPath'],
